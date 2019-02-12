Fixed variance bug (error function plot much better for linear regression), also un-normalized data for logistic regression

linear-regression.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[208]:
+# In[24]:
 
 
 import numpy as np
@@ -9,9 +9,10 @@ import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 from matplotlib import cm
+import math
 
 
-# In[209]:
+# In[25]:
 
 
 # PART A
@@ -31,11 +32,11 @@ mean = mean/m
 e_x_squared = squared_sum/m
 variance = e_x_squared - mean*mean
 
-x_i_norm = np.array([(xi-mean)/variance for xi in x_i_raw])
+x_i_norm = np.array([(xi-mean)/math.sqrt(variance) for xi in x_i_raw])
 x_i = np.array([[1, xi] for xi in x_i_norm])
 
 
-# In[210]:
+# In[26]:
 
 
 # Detect Convergence
@@ -62,7 +63,7 @@ def compute_error(theta_0, theta_1):
     return error
 
 
-# In[211]:
+# In[27]:
 
 
 # Gradient Descent
@@ -99,16 +100,16 @@ print (theta)
 print (num_iterations)
 
 
-# In[212]:
+# In[28]:
 
 
-# Plot Graphs
+# Plot Graphs for PART B
 h_theta = np.array([np.dot(theta, xi) for xi in x_i])
 plt.plot(x_i_raw, y_i, 'ro')
 plt.plot(x_i_raw, h_theta, 'b-')
 
 
-# In[213]:
+# In[29]:
 
 
 fig = plt.figure()
@@ -116,9 +117,9 @@ ax = p3.Axes3D(fig)
 
 # Plot for PART C
 # Set Axes Properties
-theta_0_min, theta_0_max = -0.5, 2.5
-theta_1_min, theta_1_max = -1.5, 1.5
-error_func_min, error_func_max = 0, 3
+theta_0_min, theta_0_max = -1, 3
+theta_1_min, theta_1_max = -2, 2
+error_func_min, error_func_max = 0, 4
 ax.set_xlim3d([theta_0_min, theta_0_max])
 ax.set_ylim3d([theta_1_min, theta_1_max])
 ax.set_zlim3d([error_func_min, error_func_max])
@@ -141,9 +142,10 @@ ani = animation.FuncAnimation(fig, update_lines, num_iterations-1, fargs=(curve,
 plt.show()
 
 
-# In[214]:
+# In[30]:
 
 
+# Plot contour for PART D
 plt.ion()
 contour_plot = plt.plot([], [], 'r.')[0]
 plt.contour(theta_0_surface, theta_1_surface, error_func_surface)

locally-weighted.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[51]:
 
 
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 
-# In[2]:
+# In[52]:
 
 
-# PART A
+# Read data from files
 x_i_raw = np.genfromtxt('./ass1_data/weightedX.csv', delimiter=',')
 y_i = np.genfromtxt('./ass1_data/weightedY.csv', delimiter=',')
 theta = np.array([0.00, 0.00])
 m = x_i_raw.size
-n = 0.0195
+n = 0.5
 
 # Normalize data
 mean = 0
@@ -28,9 +29,119 @@ mean = mean/m
 e_x_squared = squared_sum/m
 variance = e_x_squared - mean*mean
 
+x_i_unnorm = np.array([[1, xi] for xi in x_i_raw])
 x_i_norm = np.array([(xi-mean)/variance for xi in x_i_raw])
-x_i = np.array([[xi, 1] for xi in x_i_norm])
-# print (x_i_norm)
+x_i = np.array([[1, xi] for xi in x_i_norm])
+
+
+# In[53]:
+
+
+# Detect Convergence
+epsilon = 0.00000001
+def converged(theta_next, theta):
+    converged = True
+    for d in range(theta.size):
+        converged = converged and abs(theta_next[d] - theta[d]) < epsilon
+    return converged
+
+
+# In[54]:
+
+
+# Gradient Descent
+num_iterations = 0
+total_iterations = 10
+
+# Arrays ti hold parameter values for all iterations
+theta_0, theta_1, error_func = [], [], []
+curve = []
+
+while(True):
+    theta_next = np.array([0.00, 0.00])
+    j_theta = 0
+    for j in range(theta.size):
+        sum = 0
+        for i in range(m):
+            hyp = np.dot(theta, x_i[i])
+            sum = sum + x_i[i][j]*(y_i[i] - hyp)
+            j_theta = j_theta + (y_i[i] - hyp)*(y_i[i] - hyp)
+        theta_next[j] = theta[j] + n*sum/m
+        
+    if (converged(theta_next, theta)):
+        break
+        
+    j_theta = j_theta/(4*m)
+    theta_0.append(theta[0])
+    theta_1.append(theta[1])
+    error_func.append(j_theta)
+    
+    theta = theta_next
+    num_iterations += 1
+    
+print (theta)
+print (num_iterations)
+
+
+# In[55]:
+
+
+# Plot Graphs for PART A - Unweighted Linear Regression
+h_theta = np.array([np.dot(theta, xi) for xi in x_i])
+plt.plot(x_i_raw, y_i, 'ro')
+plt.plot(x_i_raw, h_theta, 'b-')
+
+
+# In[56]:
+
+
+# Function to give the predictions for the entire data set using locally weighted regression
+def locally_weighted_plot(tau):
+    prediction = []
+    for x in x_i_unnorm:
+        # Construct the weights matrix
+        w_i = []
+        for xi in x_i_unnorm:
+            weight = math.exp(-np.dot(x-xi, x-xi)/(2*tau*tau))
+            w_i.append(weight)
+        w = np.diag(w_i)
+        
+        # Calculate theta for the given x
+        x_transpose_w = np.dot(np.transpose(x_i_unnorm),w)
+        theta_left = np.linalg.inv(np.dot(x_transpose_w, x_i_unnorm))
+        theta_right = np.dot(x_transpose_w, y_i)
+        theta = np.dot(theta_left, theta_right)
+        
+        # Add to the prediction array
+        prediction.append(np.dot(theta, x))
+        
+    return (x_i_unnorm[:, [1]], prediction)
+
+
+# In[57]:
+
+
+# Plot for PART B
+tuple_1 = locally_weighted_plot(0.8)
+plt.plot(tuple_1[0], tuple_1[1], 'bo')
+
+
+# In[58]:
+
+
+# Plot graphs for PART C
+tuple_1 = locally_weighted_plot(0.1)
+tuple_2 = locally_weighted_plot(0.3)
+tuple_3 = locally_weighted_plot(2)
+tuple_4 = locally_weighted_plot(10)
+
+fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
+axs[0, 0].plot(tuple_1[0], tuple_1[1], 'b.')
+axs[0, 1].plot(tuple_2[0], tuple_2[1], 'r.')
+axs[1, 0].plot(tuple_3[0], tuple_3[1], 'g.')
+axs[1, 1].plot(tuple_4[0], tuple_4[1], 'y.')
+
+plt.show()
 
 
 # In[ ]: